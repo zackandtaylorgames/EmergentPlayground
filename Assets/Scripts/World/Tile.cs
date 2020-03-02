5390d299f0491b7daea1d3a2ad50bfea7af36a9b
Taylor Moore <zackandtaylorgames@gmail.com>
2020-03-02 00:04:11 -0800

    A world is now generated out of prefab tiles.

---
+++ Assets/Scripts/World/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 coordinates;
+    private int type;
+    private string tileName;
+
+    public void SetValues(float xCoord, float yCoord)
+    {
+        coordinates.x = xCoord;
+        coordinates.y = yCoord;
+
+        type = 0;
+        tileName = "Basic";
+    }
+}
---
+++ Assets/Scripts/World/World.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tile;
+
+    [SerializeField]
+    private int WORLD_WIDTH;
+    [SerializeField]
+    private int WORLD_HEIGHT;
+
+    [SerializeField]
+    private float WIDTH_OFFSET;
+    [SerializeField]
+    private float HEIGHT_OFFSET;
+
+    void Start()
+    {
+        GenerateWorld();
+    }
+
+    void GenerateWorld()
+    {
+        for(int j = 0; j < WORLD_HEIGHT; j++)
+        {
+            for(int i = 0; i < WORLD_WIDTH; i++)
+            {
+                GameObject newTile = Instantiate(tile, new Vector2(i + WIDTH_OFFSET, j + HEIGHT_OFFSET), Quaternion.identity);
+                newTile.GetComponent<Tile>().SetValues((float)i, (float)j);
+                newTile.transform.SetParent(gameObject.transform);
+            }
+        }
+    }
+}
